604aee41a0adef6cb612d7aaf4592790388db05d
Fluke <adisorn.sjr@gmail.com>
2022-12-14 13:22:47 +0700

    fi

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -3,40 +3,32 @@
 using namespace std;
 
 int main(){
-	int i = 0, P[5000], k, result = 0;
+	
+	int i = 0, P[5000], k;
 	cout << "Please input k: ";
-	cin >> k;
-	while(i < 5000){
+    cin >> k;
+    while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
 	}
-	if(k > 0){
-		int i = 0;
-		result = 0;
-		while(i < 5000){
-		if(i < 5000){
-			
-			if(P[i]%k == 0){
-				result = result-P[i];
-				
-			}else{
-				result = result+P[i];
-				
-			}
-
-		}else{
-			cout << result;
-		}
-		i++;
-		}
-		cout << "Result = " << result;
-	
-	}else{
-		cout << "Invalid input!!!";
-
-	}
-
-
-
-	return 0;
+    if(k > 0){
+        int i = 0;
+        int result = 0;
+        while(i < 5000){
+            if(i < 5000){
+                if(P[i]%k == 0){
+                    result = result - P[i];
+                }else{
+                    result = result + P[i];
+                }
+             }else{
+                cout << result;
+            }
+            i++;        
+        }  
+        cout << "Result = " << result;
+    }else{
+        cout << "Invalid input!!!";
+    }
+    return 0;
 }
